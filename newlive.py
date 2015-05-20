d0dd6fdc6ac31abcc5078a5dc024ea3e4cf6361f
solflare1 <lucas.nexus@gmail.com>
2015-05-20 14:09:34 -0700

    Updates

--- newlive.py
+++ newlive.py
@@ -31,7 +31,7 @@ headNodes = {}
 curLine = 0
 with open(filename, 'r') as f:
     for line in f:
-    
+        
         print str(curLine) + '|' + line.rstrip()
     
         ## IF STATEMENTS
@@ -43,7 +43,7 @@ with open(filename, 'r') as f:
             readVariables = []
             readVariables.extend(expression)
             
-            node = Node(line, readVariables,"if")
+            node = Node(line, expression,"if")
             node.gotoNumber = gotoLine
             nodes.append(node)
     #      print ">>CREATED TOKEN: IF goto " + token.gotoLabelNumber + " readings> " + str(token.readVariables)
@@ -64,7 +64,7 @@ with open(filename, 'r') as f:
         if re.search('^ goto.', line):
             expression = re.search('(?<=Label)\d+', line)
             gotoLine = expression.group(0)
-            node = Node(line, readVariables, "goto")
+            node = Node(line, set(), "goto")
             node.gotoNumber = gotoLine
             nodes.append(node)
                 #     print ">>CREATED TOKEN: IF goto " + token.gotoLabelNumber
@@ -78,7 +78,7 @@ with open(filename, 'r') as f:
                 readVariables = []
                 readVariables.extend(expression)
 
-            node = Node(line, readVariables, "assign")
+            node = Node(line, expression, "assign")
             node.variable = variable
             nodes.append(node)
                             #      print ">>CREATED TOKEN: VARIABLE: ASSIGNED>" + token.variable + " reading>" + str(token.readVariables)
@@ -88,15 +88,17 @@ with open(filename, 'r') as f:
             expression = re.findall('([a-z]|[A-Z])', line)
             node = Node(line, expression, "LAST")
             nodes.append(node)
+        print(str(node.liveBefore))
+        readVariables = []
 print(str(len(nodes)) + "***********************")
 
 count = 0
 blocks = []
 #node previous and block creation, and graph head node look up table FIRST TRAVERSAL
 for node in nodes:
-    if(count - 1 >= 0):
-        node.prev.add(nodes[count-1])
-
+    #if(count - 1 >= 0):
+    #   node.prev.add(nodes[count-1])
+    print(str(node.liveBefore) + " BEFORE UPDATE")
     if(node is nodes[0] and node.type is not "label" and node.type is not "goto" and node.type is not "if"):
         block = Block()
         block.headNode = node
@@ -114,43 +116,44 @@ for node in nodes:
         if(count + 1 < len(nodes)):
             block.headNode = nodes[count+1]
         blocks.append(block)
+    print(str(node.liveBefore) + " AFTER UPDATE")
 
     count += 1
 
 #block previous and tail
 count = 0
 print(headNodes)
+    #for node in nodes:
+    #if(node.type is "assign"):
+    #   if(count+1 < len(nodes)):
+    #       nodes[count+1].prev.add(node)
+
+#    if(node.type is "if"):
+#       if(count+1 < len(nodes)):
+#           nodes[count+1].prev.add(node)
+#       print("GO TO: " + node.gotoNumber)
+#       headNodes[node.gotoNumber].prev.add(node)
+
+#   if(node.type is "goto"):
+#       headNodes[node.gotoNumber].prev.add(node)
+
+
+
+
+#count += 1
+#nodes.reverse()
+
 for node in nodes:
-    if(node.type is "assign"):
-        if(count+1 < len(nodes)):
-            nodes[count+1].prev.add(node)
-
-    if(node.type is "if"):
-        if(count+1 < len(nodes)):
-            nodes[count+1].prev.add(node)
-        print("GO TO: " + node.gotoNumber)
-        headNodes[node.gotoNumber].prev.add(node)
-
-    if(node.type is "goto"):
-        headNodes[node.gotoNumber].prev.add(node)
-
-
-    count += 1
-count = len(nodes)-1
-node = nodes[count]
-while(count >= 0):
     for prevNode in node.prev:
         if(node.variable is not None and node.variable in prevNode.liveAfter):
             prevNode.liveAfter.remove(node.variable)
         if(node.liveBefore is not None):
-            #print(str(node.liveBefore) + " BEFORE UPDATE")
+            print(str(node.liveBefore) + " BEFORE UPDATE")
             prevNode.liveAfter.update(node.liveBefore)
-            #print(str(node.liveBefore) + " AFTER UPDATE")
-    count -= 1
-    node = nodes[count]
+            print(str(node.liveBefore) + " AFTER UPDATE")
 
-
-
+print(nodes[0].prev)
+#nodes.reverse()
 for node in nodes:
     if(node.type is "label"):
         print(node.value.rstrip())
